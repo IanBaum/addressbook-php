a8d05dd4c2ac782eb2751c1b3cad1b3fd290d044
Ian Baum <ian@ianbaum.com>
2016-09-09 08:59:55 -0400

    add ability to delete contact list

--- app/app.php
+++ app/app.php
@@ -4,9 +4,9 @@
 
     session_start();
     if(empty($_SESSION['list_of_contacts'])){
-      $_SESSION['list_of_contacts'] = array();
+        $_SESSION['list_of_contacts'] = array();
     }
-
+    //silex and twig setup
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider(),array(
         'twig.path' => __DIR__.'/../views'
@@ -15,11 +15,17 @@
     $app->get('/', function() use($app){
         return $app['twig']->render('home.html.twig', array('contacts'=> Contact::getAll()));
     });
-
+    //Add contact Page
     $app->post('/addcontact', function() use($app){
-      $contact = new Contact($_POST['contactAddress'],$_POST['contactName'],$_POST['contactPhone']);
-      $contact->save();
-      return $app['twig']->render('addcontact.html.twig', array('newcontact' => $contact));
+        $contact = new Contact($_POST['contactAddress'],$_POST['contactName'],$_POST['contactPhone']);
+        $contact->save();
+        return $app['twig']->render('addcontact.html.twig', array('newcontact' => $contact));
+    });
+    //Remove contact Page
+    $app->post('/delete_contacts', function() use($app){
+        Contact::deleteAll();
+
+        return $app['twig']->render('deletecontacts.html.twig');
     });
 
     return $app;
